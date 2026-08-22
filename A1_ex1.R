f4210bc5720a72f755e92d56155aa8603b0a992d
ratanaksophea <ratanak.sophea@students.mq.edu.au>
2026-08-23 00:38:17 +1000

    Attempted to calculate total electricity demanded

--- A1_ex1.R
+++ A1_ex1.R
@@ -1,3 +1,15 @@
-# Question 1
+# Exercise 1
+# Load Library
 library(readr)
-x<- read.csv("vic_elect.csv")
+library(tidyverse)
+
+# Q1, Import data set
+x <- read.csv("vic_elect.csv")
+
+# Q2, Display structure of X
+glimpse(x)
+
+#Q3, Calculate total electricity demanded and print
+total_electricity_demand <- x %>%
+  summarise(total_demand = sum(demand, na.rm = TRUE))
+
